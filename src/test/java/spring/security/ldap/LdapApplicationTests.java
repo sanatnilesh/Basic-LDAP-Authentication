cc037e2f0f41643d27c1fd6c7992bae14de6c17e
sanatnilesh <sanat541997@gmail.com>
2023-03-11 13:03:42 -0500

    authentication-ldap-example

---
+++ src/test/java/spring/security/ldap/LdapApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security.ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
